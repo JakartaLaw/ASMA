9c5fe8947ab65353028d942772400dcfd8497fd4
JonasPC <sonyogi@hotmail.com>
2018-03-07 13:09:11 +0000

    utils + cleandata forbedret

--- ass2/modules/cleandata.py
+++ ass2/modules/cleandata.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from datetime import datetime
+from modules.utils import date_fixer
 
 
 class CleanData(object):
@@ -11,9 +11,10 @@ class CleanData(object):
         data = pd.read_csv('data_and_factors.csv', sep=';', decimal=',',
                            header=None, usecols=[0, 1, 2, 3, 4, 5])
         data.columns = ['date', 'MKT', 'SMB', 'HML', 'WML', 'Tbillrate']
+        data['date'] = date_fixer(data['date'])
         #data.set_index('date', inplace=True)
 
-        return data
+        return data['date']
 
     @classmethod
     def get_data(cls, csv_name):
@@ -22,11 +23,5 @@ class CleanData(object):
         data = pd.read_csv(csv_name, sep=';', decimal=',',
                            header=None)
 
-        #data.set_index(index, inplace=True)
+        data.set_index(index, inplace=True)
         return data
-
-
-#data = CleanData.get_data('bmit.csv')
-#data = CleanData.get_index()
-#data.head()
-#pd.to_datetime(str(data['date']), format='%Y%m')
