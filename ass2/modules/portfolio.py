70774441e3f5144e2b03f9221da8aa36ceb1a3bf
JonasPC <sonyogi@hotmail.com>
2018-03-10 10:54:35 +0000

    lavet et første draft på Portfolio, der virker

---
+++ ass2/modules/portfolio.py
@@ -0,0 +1,35 @@
+class Portfolio(object):
+    """
+    Takes a list of period-wise returns of different stocks and converts them into a portfolio return
+    """
+    
+    def __init__(self):
+        pass
+    
+    def portfolio_weigths(self, df):
+        _list = []
+        for i in range(len(df)):
+            w = 1/df.count(axis=1)[i]
+            _list.append(w)
+        return _list
+    
+    @classmethod
+    def period_return(cls, df):
+        _portfolio = df.sum(axis=1)
+        _w = cls.portfolio_weigths(cls, df)
+        new_list = [_w[i]*_portfolio[i] for i in range(len(_w))]
+        return new_list
+            
+
+#### test ####
+#import pandas as pd
+#import numpy as np
+#df = pd.DataFrame([[1,2,3,np.nan],[np.nan,3,2,3]])
+#print(df.count(axis=1)[0])
+#ny = Portfolio()
+#a = list(df.sum(axis=1))
+#b = ny.portfolio_weigths(df)
+#new_list = [a[i]*b[i] for i in range(len(a))]
+#print(ny.period_return(df))
+        
+    
