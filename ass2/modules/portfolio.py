ee20e6451c84e96594d95e2771f602abef07a4cb
JonasPC <sonyogi@hotmail.com>
2018-03-10 11:53:00 +0000

    bla bla

--- ass2/modules/portfolio.py
+++ ass2/modules/portfolio.py
@@ -15,16 +15,17 @@ class Portfolio(object):
         return _list
     
     @classmethod
-    def period_return(cls, df):
+    def portfolio_return(cls, df):
         _portfolio = df.sum(axis=1)
         _w = cls.portfolio_weigths(df)
         new_list = [_w[i]*_portfolio[i] for i in range(len(_w))]
         new_df = pd.DataFrame(new_list, index=df.index)
         return new_df
     
-    def __init__(self, df):
-        self.df_portfolio = self.period_return(df)
+    #def __init__(self, df):
+        #self.df_portfolio = self.period_return(df)
         
-    def get_portfolio(self):
-        return self.df_portfolio
+   # def get_portfolio(self):
+    #    return self.df_portfolio
             
+
