7a0fc8ea13b787362881c9a9fd891fae9384a64d
JakartaLaw <jeppesjohansen@gmail.com>
2018-03-10 12:36:23 +0000

    done with performance

--- ass2/modules/performance.py
+++ ass2/modules/performance.py
@@ -1,23 +1,60 @@
 import pandas as pd
 import numpy as np
+#from modules.utils import to_latex_table
+
 
 class Performance(object):
-    
-    def __init__(self):
-        pass
-    
-    def geo_return(self, df):
-        my_list = []
-        for i in range(len(df[0])):
-            _rr = df[0][i]+1
-            my_list.append(_rr)
-        _n = df[0].count()
-        geo_return = np.prod(np.array(my_list))**(1/_n) -1
-        return geo_return 
-        
-    def ar_return(self, df):
-        _sum = df[0].sum()
-        _n = df[0].count()
-        return _sum/_n
-    
 
+    # avg-return
+    # geo-mean
+    # std_dev
+    # sharpe
+    # t_stat
+
+    def __init__(self, portfolio_returns, frequency, risk_free_rate):
+        self.portfolio_returns
+        self.frequency
+        self.risk_free_rate
+
+    @staticmethod
+    def avg_return(series, frequency):
+        return np.mean(series * frequency)
+
+    @staticmethod
+    def std_dev(series, frequency):
+        return np.sqrt(np.var(VolatilityTimingPortfolio) * frequency)
+
+    @classmethod
+    def sharp_ratio(cls, series, frequency, risk_free_rate):
+        ER = cls.avg_return(series, frequency)
+        STD = cls.avg_return(series, frequency)
+        return (ER - np.mean(risk_free_rate)) / STD
+
+    @staticmethod
+    def t_statistic(series, frequency):
+
+        beta = np.mean(series) * frequency
+        sigma = np.std(series) * np.sqrt(frequency)
+        sqrt_n = np.sqrt(len(series))
+
+        sigma_beta = sigma / sqrt_n
+
+        return beta / sigma_beta
+
+    @staticmethod
+    def geo_return(series, frequency):
+        product_array = np.array([i + 1 for i in series])
+        return np.prod(product_array)**(frequency / len(series)) - 1
+
+    def get_performance(self):
+        avg_ret = self.avg_return(self.series, self.frequency)
+        std_dev = self.std_dev(self.series, self.frequency)
+        t_stat = self.t_statistic(self.series, self.frequency)
+        geo_ret = self.geo_return(self.series, self.frequency)
+
+        self.performance = pd.DataFrame({'avg return': avg_ret, 'std. deviation': std_dev})
+        return self.performance
+
+    def performance_to_latex(self, file_name):
+        #to_latex_table(file_name, self.performance, directory=None, index=False, nr_decimals=2)
+        pass
--- ass2/modules/utils.py
+++ ass2/modules/utils.py
@@ -4,15 +4,33 @@ def date_fixer(x):
     """
     x = column of dates formatted yyyymm
     """
-    
+
     list_year = []
     for i in range(len(x)):
         s = str(x[i])
         y = s[0:4]
         m = s[4:6]
-    
-        list_year.append('{}-01-{}'.format(m,y))
+
+        list_year.append('{}-01-{}'.format(m, y))
 
     years_w = pd.to_datetime(list_year)
     return years_w
 
+
+def to_latex_table(file_name, df, directory=None, index=False, nr_decimals=2):
+    """
+    Parameters
+    ==========
+    file_name : name of table (without .tex)
+    df : pandas.DataFrame (object) which has to be rendered to latex
+    index : should the index of the DataFrame be shown
+    """
+
+    df = df.round(nr_decimals)
+
+    if directory is None:
+        with open('{}.tex'.format(file_name), 'w') as tf:
+            tf.write(df.to_latex(index=index))
+    else:
+        with open('{}//{}.tex'.format(directory, file_name), 'w') as tf:
+            tf.write(df.to_latex(index=index))
